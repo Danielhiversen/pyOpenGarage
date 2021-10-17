542a872283ed91897714b18c71eda09f201b07ef
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-10-17 21:26:52 +0200

    0.1.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         "aiohttp>=3.0.6",
         "async_timeout>=1.4.0",
     ],
-    version="0.1.5",
+    version="0.1.6",
     description="A python3 library to communicate with Open Garage",
     python_requires=">=3.5.3",
     author="Daniel Hjelseth Høyer",
